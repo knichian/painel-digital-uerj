ad39d2b92735daf829683e6054b54eacd9e5b276
Gabriel César <knichian@gmail.com>
2026-07-05 08:43:15 -0300

    feat(backend): adicionando script bash para rodar aplicação no servidor

---
+++ app_launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SESSION_NAME='painel-uerj'
+
+tmux new-session -d -s "${SESSION_NAME}"
+
+tmux new-window -t "${SESSION_NAME}" -n 'server'
+tmux new-window -t "${SESSION_NAME}" -n 'client'
+
+tmux send-keys -t "${SESSION_NAME}:server" 'cd server && source ${HOME}/.nvm/nvm.sh && nvm install 20 && node index.js' Enter
+tmux send-keys -t "${SESSION_NAME}:client" 'cd client &&  source ${HOME}/.nvm/nvm.sh && nvm install 20 && npm run build && npm run preview -- --host' Enter
